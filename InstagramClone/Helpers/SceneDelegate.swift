759c6704d9760a852f5d4f9c537e86240286c52b
Nilesh Kumar <k.nilesh010@gmail.com>
2022-02-10 02:28:24 +0530

    install dependecies

--- InstagramClone/Helpers/SceneDelegate.swift
+++ InstagramClone/Helpers/SceneDelegate.swift
@@ -14,10 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        guard let scene = scene as? UIWindowScene else { return }
-        window = UIWindow(windowScene: scene)
-        window?.rootViewController = TabsViewController()
-        window?.makeKeyAndVisible()
+        guard let _ = (scene as? UIWindowScene) else { return }
+       // window?.rootViewController = TabsViewController()
+       // window?.makeKeyAndVisible()
         
     }
 
